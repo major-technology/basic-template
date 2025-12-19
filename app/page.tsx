074b753fbc6770678f37b9654c877552b5c44c28
josegironn <30703536+josegironn@users.noreply.github.com>
2025-12-18 17:27:24 -0800

    Upgrade to nextjs 16

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
+              className="dark:invert w-5 h-5"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
