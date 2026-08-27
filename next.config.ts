db2cfa0fad88e4443ec51535ee095c58a6e8120e
Vlad <vlad@majortechnology.com>
2026-08-27 16:39:02 +0200

    Add trusted dev origins to the Next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["**.major.build", "**.localhost"],
   output: "standalone",
   productionBrowserSourceMaps: true,
   experimental: {
